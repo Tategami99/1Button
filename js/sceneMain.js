1a7cceb4cc03b8b7c500d847b86ba8c0fb4b5dde
Tategami99 <k07bautista@gmail.com>
2022-11-22 17:45:20 -0800

    setup

---
+++ js/sceneMain.js
@@ -0,0 +1,16 @@
+class SceneMain extends Phaser.Scene {
+    constructor() {
+        super('SceneMain');
+    }
+    preload()
+    {
+    	//loads images or sounds
+    }
+    create() {
+        console.log("Ready!");
+        //defines objects
+    }
+    update() {
+        //checks for collisions and updates values 
+    }
+}
